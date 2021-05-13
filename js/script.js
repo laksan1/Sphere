c5d3ec422df753ee8ea8bf4d6c1eae0a42eed021
a.a.laktionov <a.a.laktionov@in-delo.com>
2021-05-13 19:37:40 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,46 @@
+window.onload = function(){
+
+    
+var width = window.innerWidth;
+var height = window.innerHeight;
+var canvas = document.getElementById('canvas');
+
+canvas.setAttribute('width',width);
+canvas.setAttribute('height',height);
+
+var renderer = new THREE.WebGLRenderer({canvas:canvas});
+renderer.setClearColor(0x000000);
+var scene = new THREE.Scene();
+
+
+var camera = new THREE.PerspectiveCamera(45, width/height, 0.1,5000);
+camera.position.set(100,0,1000);
+
+var light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
+
+var geometry = new THREE.SphereGeometry(200,12,12);
+var material = new THREE.MeshBasicMaterial({color:0xffff00,vertexColors: THREE.FaceColors, wireframe: true});//Пустотелый
+
+for(var i = 0; i < geometry.faces.lenght; i++){
+
+geometry.faces[i].color.setRGB(Math.random(),Math.random(),Math.random());
+
+}
+
+var mesh = new THREE.Mesh(geometry,material);
+scene.add(mesh);
+
+renderer.render(scene, camera);
+
+function loop (){
+
+    mesh.rotation.y  += Math.PI / 500;
+    renderer.render(scene,camera);
+    requestAnimationFrame(function(){loop();});
+
+
+}
+loop();
+
+}
